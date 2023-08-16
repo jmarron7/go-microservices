f55523656243eb1f459160934c611b42496900bd
Jesus Marron <jesusmarron7@gmail.com>
2023-08-16 16:00:43 -0500

    added http error handling

--- main.go
+++ main.go
@@ -11,7 +11,11 @@ func main() {
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		log.Println("Hello World")
-		d, _ := ioutil.ReadAll(r.Body)
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Oops", http.StatusBadRequest)
+			return
+		}
 		fmt.Fprintf(rw, "hello %s", d)
 	})
 
